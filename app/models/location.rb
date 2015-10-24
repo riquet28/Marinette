0ce64d258db8e54aac48f2607eb951c1ce2c03be
riquet <eric.merlin3@gmail.com>
2015-10-25 01:36:44 +0200

    Initial commit - Projet déjà bien avancé

---
+++ app/models/location.rb
@@ -0,0 +1,20 @@
+class Location < ActiveRecord::Base
+	geocoded_by :address
+	after_validation :geocode, :if => :address_changed?
+
+	def wind
+		json = ForecastIO.forecast(self.latitude, self.longitude, params: { units: 'si' })
+		json.currently.windSpeed
+	end
+
+	def temperature
+		json = ForecastIO.forecast(self.latitude, self.longitude, params: { units: 'si' })
+		json.currently.temperature.to_i
+	end
+
+	def ciel
+		json = ForecastIO.forecast(self.latitude, self.longitude)
+		json.currently.summary
+	end
+
+end
