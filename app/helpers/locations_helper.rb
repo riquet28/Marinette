dd704400e2dfe6e4f5a562ee9b132d004260d90a
riquet <eric.merlin3@gmail.com>
2015-10-29 23:30:22 +0100

    Utilisation des helpers pour la mise en place des panneaux infos en fonction des températures

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,14 +1,38 @@
 module LocationsHelper
 
 	def quelle_tenue(selontemperature)
-		if selontemperature.temperature >= 25
-			image_tag("maillot_bain.jpg")
-		elsif selontemperature.temperature >= 10 && selontemperature.temperature <= 24
-			image_tag("short.jpg")
-		elsif selontemperature.temperature >= 0 && selontemperature.temperature <= 9
-			"Il fait froid, couvrez-vous !"
+		if selontemperature >= 25
+			'Maillot de bain !!!'
+		elsif selontemperature >= 10 && selontemperature <= 24
+			'Short et Sweet !!!'
+		elsif selontemperature >= 0 && selontemperature <= 9
+			"Pantalon et Manteau !!!"
 		else
-			image_tag("pere_noel.jpg")
+			'Restez chez vous !!!'
+		end
+	end
+
+	def quel_accessoire(accessoire)
+		if accessoire >= 25
+			'Serviette et crème solaire !!!'
+		elsif accessoire >= 10 && accessoire <= 24
+			'Ballon et jeux de plage !!!'
+		elsif accessoire >= 0 && accessoire <= 9
+			"Prevoyez les gants !!!"
+		else
+			'Rien, vous êtes chez vous !!!'
+		end
+	end
+
+	def quelle_activite(activite)
+		if activite >= 25
+			'Baignade et bronzage!!!'
+		elsif activite >= 10 && activite <= 24
+			'Bronzage et pêche à pieds !!!'
+		elsif activite >= 0 && activite <= 9
+			"Bougez il fait froid !!!"
+		else
+			'Rien, vous êtes chez vous !!!'
 		end
 	end
 
